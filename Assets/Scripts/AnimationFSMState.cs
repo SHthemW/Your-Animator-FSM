b69e4ca973f18aa64c564700a621eec4029cf5f4
S.H.W <2536076378@qq.com>
2024-01-07 21:34:49 +0800

    dev: state develop

--- Assets/Scripts/AnimationFSMState.cs
+++ Assets/Scripts/AnimationFSMState.cs
@@ -1,18 +1,73 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimationFSMState : MonoBehaviour
+namespace Yours.AnimationFSM
 {
-    // Start is called before the first frame update
-    void Start()
+    public abstract class AnimationFSMState : StateMachineBehaviour
     {
-        
-    }
+        protected Animator _animator { get; private set; }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private bool _isInited = false;
+        private bool _InitFailed = false;
+
+        public override sealed void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            if (!_isInited)
+            {
+                try
+                {
+                    Init(animator);
+                }
+                catch (Exception e)
+                {
+                    _InitFailed = true;
+                    throw e;
+                }
+                _isInited = true;
+            }
+
+            if (_InitFailed)
+                return;
+
+            base.OnStateEnter(animator, stateInfo, layerIndex);
+            EnterStateAction();
+        }
+
+        public override sealed void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            if (_InitFailed)
+                return;
+
+            base.OnStateUpdate(animator, stateInfo, layerIndex);
+            UpdateStateAction();
+        }
+
+        public override sealed void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            if (_InitFailed)
+                return;
+
+            base.OnStateExit(animator, stateInfo, layerIndex);
+            ExitStateAction();
+        }
+
+        protected virtual void Init(Animator obj)
+        {
+            _animator = obj;
+        }
+        protected virtual void EnterStateAction() { }
+        protected virtual void UpdateStateAction() { }
+        protected virtual void ExitStateAction() { }
+
+        protected static TBehv GetBehaviour<TBehv>(Animator obj, bool must = true) where TBehv : IBehaviourHolder
+        {
+            var component = obj.GetComponentInParent<TBehv>();
+
+            if (component == null && must)
+                throw new MissingComponentException(typeof(TBehv).Name);
+
+            return component;
+        }
     }
 }
---
+++ Assets/Scripts/IBehaviourHolder.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Yours.AnimationFSM
+{
+    public interface IBehaviourHolder
+    {
+
+    }
+}
